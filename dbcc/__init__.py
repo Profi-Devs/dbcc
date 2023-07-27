402e05e17c7c4b0c49f7dbbf04db9328e750a058
Stepan Starovoitov <dark989826@gmail.com>
2023-07-27 13:43:13 +0300

    change version

--- dbcc/__init__.py
+++ dbcc/__init__.py
@@ -9,7 +9,7 @@
     :copyright: (c) 2023 by Stepan Starovoitov.
     :license: BSD, see LICENSE for more details.
 """
-__version__ = "0.0.1.dev1"
+__version__ = "0.0.1.dev2"
 
 from dbcc.base import TableEngine
 from dbcc.mongodb import MongoTableEngine
