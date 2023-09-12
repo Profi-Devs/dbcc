917c952da386293c5927714af7e3e7b58e44c7c2
Stepan Starovoitov <dark989826@gmail.com>
2023-09-12 03:13:12 +0300

    uuidRepresentation

--- dbcc/mongodb.py
+++ dbcc/mongodb.py
@@ -31,7 +31,9 @@ class MongoTableEngine(TableEngine):
 
         if not mongo:
             if not mock_db_in_mem:
-                mongo = AsyncIOMotorClient(url, connect=False)
+                mongo = AsyncIOMotorClient(
+                    url, connect=False, uuidRepresentation="standard"
+                )
                 mongo.get_io_loop = asyncio.get_event_loop
             else:
                 from mongomock_motor import AsyncMongoMockClient
